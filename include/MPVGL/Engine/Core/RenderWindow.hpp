5b6ccd87c464611aa1c92fc28dab228a40258aa7
Jan Górski <jan.a.gorski@wp.pl>
2025-07-28 15:50:32 +0200

    Added RenderWindow class and removed Vulkan Initialization from main file.

--- include/MPVGL/Engine/Core/RenderWindow.hpp
+++ include/MPVGL/Engine/Core/RenderWindow.hpp
@@ -1,8 +1,34 @@
 #pragma once
 
-#include <iostream>
+#include <string>
+
+#include <GLFW/glfw3.h>
+
+#include "MPVGL/Engine/Core/Vulkan.hpp"
 
 namespace mpvgl {
-void hello();
-}
 
+class RenderWindow {
+public:
+  explicit RenderWindow(int width, int height, std::string const &title,
+                        GLFWmonitor *monitor = nullptr,
+                        GLFWwindow *share = nullptr) noexcept(false);
+
+  RenderWindow(RenderWindow const &other) noexcept = delete;
+  RenderWindow(RenderWindow &&other) noexcept;
+
+  RenderWindow &operator=(RenderWindow const &other) noexcept = delete;
+  RenderWindow &operator=(RenderWindow &&other) noexcept;
+
+  ~RenderWindow() noexcept;
+
+public:
+  int draw() noexcept;
+
+private:
+  Init init;
+  RenderData render_data;
+  GLFWwindow *window;
+};
+
+} // namespace mpvgl
--- src/MPVGL/Engine/Core/RenderWindow.cpp
+++ src/MPVGL/Engine/Core/RenderWindow.cpp
@@ -1,9 +1,69 @@
+#include <stdexcept>
+#include <string>
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 #include "MPVGL/Engine/Core/RenderWindow.hpp"
+#include "MPVGL/Engine/Core/Vulkan.hpp"
 
 namespace mpvgl {
 
-void hello() { std::cout << "Hello World!\n"; }
+RenderWindow::RenderWindow(int width, int height, std::string const &title,
+                           GLFWmonitor *monitor,
+                           GLFWwindow *share) noexcept(false)
+    : init(), render_data() {
+
+  if (0 != device_initialization(init))
+    throw std::runtime_error("");
+  if (0 != create_swapchain(init))
+    throw std::runtime_error("");
+  if (0 != get_queues(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_render_pass(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_graphics_pipeline(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_framebuffers(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_command_pool(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_command_buffers(init, render_data))
+    throw std::runtime_error("");
+  if (0 != create_sync_objects(init, render_data))
+    throw std::runtime_error("");
+}
+
+RenderWindow::RenderWindow(RenderWindow &&other) noexcept
+    : window(other.window), init(other.init), render_data(other.render_data) {
+  other.window = nullptr;
+  other.init = Init();
+  other.render_data = RenderData();
+}
+
+RenderWindow &RenderWindow::operator=(RenderWindow &&other) noexcept {
+  this->window = other.window;
+  other.window = nullptr;
+  this->init = other.init;
+  other.init = Init();
+  this->render_data = other.render_data;
+  other.render_data = RenderData();
+  return (*this);
+}
+
+RenderWindow::~RenderWindow() noexcept { cleanup(init, render_data); }
+
+int RenderWindow::draw() noexcept {
+  while (!glfwWindowShouldClose(init.window)) {
+    glfwPollEvents();
+    int res = draw_frame(init, render_data);
+    if (res != 0) {
+      std::cout << "failed to draw frame \n";
+      return -1;
+    }
+  }
+  init.disp.deviceWaitIdle();
+  return 0;
+}
 
 } // namespace mpvgl
