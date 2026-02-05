ee71b13df84b38db4eada678c771723358985aff
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-02-05 16:45:11 +0900

    Added second rectangle to draw.

--- src/MPVGL/Core/Vulkan.cpp
+++ src/MPVGL/Core/Vulkan.cpp
@@ -36,9 +36,19 @@ const std::vector<Vertex> vertices = {
     {{0.5f, -0.5f, 0.0f}, Color::literal::Green, {1.0f, 0.0f}},
     {{0.5f, 0.5f, 0.0f}, Color::literal::Blue, {1.0f, 1.0f}},
     {{-0.5f, 0.5f, 0.0f}, Color::literal::White, {0.0f, 1.0f}},
+
+    {{-0.5f, -0.5f, -0.5f}, Color::literal::Red, {0.0f, 0.0f}},
+    {{0.5f, -0.5f, -0.5f}, Color::literal::Green, {1.0f, 0.0f}},
+    {{0.5f, 0.5f, -0.5f}, Color::literal::Blue, {1.0f, 1.0f}},
+    {{-0.5f, 0.5f, -0.5f}, Color::literal::White, {0.0f, 1.0f}},
 };
 
-const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
+// clang-format off
+const std::vector<uint16_t> indices = {
+    0, 1, 2, 2, 3, 0,
+    4, 5, 6, 6, 7, 4
+};
+// clang-format on
 
 };  // namespace
 
