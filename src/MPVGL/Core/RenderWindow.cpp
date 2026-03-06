06b38cfd44e99e4f368c434743341c1f94a0ef41
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-03-07 00:00:34 +0900

    Added key input.

--- src/MPVGL/Core/RenderWindow.cpp
+++ src/MPVGL/Core/RenderWindow.cpp
@@ -69,19 +69,82 @@ int RenderWindow::draw() noexcept {
     std::jthread watcherThread(
         [&](std::stop_token st) { shader_watcher.run(st); });
 
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+
+    double lastX = 400.0, lastY = 300.0;
+    bool firstMouse = true;
+
+    glfwSetInputMode(vulkan.deviceContext.window, GLFW_CURSOR,
+                     GLFW_CURSOR_DISABLED);
+
     int time = 0;
     while (!glfwWindowShouldClose(vulkan.deviceContext.window)) {
         glfwPollEvents();
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_ESCAPE) ==
+            GLFW_PRESS)
+            glfwSetWindowShouldClose(vulkan.deviceContext.window, true);
+
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_W) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Forward,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_S) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Backward,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_A) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Left,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_D) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Right,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_SPACE) ==
+            GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Up,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_LEFT_SHIFT) ==
+            GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::Down,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_Q) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(CameraMovement::RollLeft,
+                                                       deltaTime);
+        if (glfwGetKey(vulkan.deviceContext.window, GLFW_KEY_E) == GLFW_PRESS)
+            vulkan.sceneContext.camera.processKeyboard(
+                CameraMovement::RollRight, deltaTime);
+
+        double xpos, ypos;
+        glfwGetCursorPos(vulkan.deviceContext.window, &xpos, &ypos);
+
+        if (firstMouse) {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos;
+        lastX = xpos;
+        lastY = ypos;
+
+        vulkan.sceneContext.camera.processMouseMovement(xoffset, yoffset);
+
         int res = vlk::draw_frame(vulkan);
         if (res != 0) {
             std::cout << "failed to draw frame \n";
             return -1;
         }
+
         if (time++ >= 10'000) {
             vlk::reloadShadersAndPipeline(vulkan);
             time = 0;
         }
     }
+
     vulkan.deviceContext.logDevDisp.deviceWaitIdle();
     return 0;
 }
